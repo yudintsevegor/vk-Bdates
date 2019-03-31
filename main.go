94b61b663d05067fc2ce42f86018cecc5dba8046
Egor <yudintsevegor@gmail.com>
2019-03-31 16:15:13 +0300

    readme added//opportunity to download *.ics file

--- main.go
+++ main.go
@@ -1,95 +1,203 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"bufio"
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/Dimonchik0036/vk-api"
 )
-type Handler struct{
-	
-}
-type FromResp struct{
-	Token string `json:"access_token"`
-	Expires string
-	UserId int `json:"user_id"`
+
+type Handler struct {
+	IcsFields []string
 }
 
-var url = "https://oauth.vk.com/authorize?client_id="+key+"&display=page&response_type=code&redirect_uri=http://127.0.0.1:8080/result&scope=friends,offline&v=5.52"
-//var url = "https://oauth.vk.com/authorize?client_id=6920031&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.52"
-func (h *Handler) handleMain(w http.ResponseWriter, r *http.Request){
-	
+type FromResp struct {
+	Token  string `json:"access_token"`
+	UserId int64  `json:"user_id"`
 }
 
-func (h *Handler) handleResult(w http.ResponseWriter, r *http.Request){
-	CODE := r.FormValue("code")
-	fmt.Println(CODE)
-	var urlToken = "https://oauth.vk.com/access_token?client_id="+key+"&client_secret="+CLIENT_SECRET+"&redirect_uri=http://127.0.0.1:8080/result&code="+CODE
+var (
+	htmlIndex = `
+		<html>
+		<head>
+		</head>
+		<body>
+			<div align="center">
+			<a href="/login">vk Log in</a>
+			</div>
+		</body>
+		</html>`
+	host = "http://127.0.0.1:8080"
+	url  = "https://oauth.vk.com/authorize?client_id=" + APP_ID + "&display=page&response_type=code&redirect_uri=" + host + "/result&scope=friends,offline&v=5.52"
+)
 
+func (h *Handler) handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, htmlIndex)
+}
 
-//	http.Redirect(w, r, urlToken, http.StatusTemporaryRedirect)
-//	return
-	resp, err :=  http.Get(urlToken)
+type ND struct {
+	Name string
+	Date string
+	Year string
+}
+
+func (h *Handler) handleResult(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	urlToken := "https://oauth.vk.com/access_token?client_id=" + APP_ID + "&client_secret=" + CLIENT_SECRET + "&redirect_uri=" + host + "/result&code=" + code
+
+	resp, err := http.Get(urlToken)
 	body, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("BODY:", string(body))
-	var st = FromResp{}
-	err = json.Unmarshal(body,&st)
-	if err != nil{
+
+	st := FromResp{}
+	err = json.Unmarshal(body, &st)
+	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(st)
+	//	fmt.Fprintf(w, "ACCESS_TOKEN: %v\nUser_Id: %v\n", st.Token, st.UserId)
+
+	client, err := vkapi.NewClientFromToken(st.Token)
+	if err != nil {
+		fmt.Println(err)
+	}
+	/*
+		hints - sort by rating
+		nom - Nominative
+	*/
+	var offset int64 = 0
+	var counts int64 = 5000
+	users := make([]ND, 0, 1)
+	now := "2019"
+
+	zeroNum := map[string]string{
+		"1": "01",
+		"2": "02",
+		"3": "03",
+		"4": "04",
+		"5": "05",
+		"6": "06",
+		"7": "07",
+		"8": "08",
+		"9": "09",
+	}
+	friends, err := client.GetFriends(st.UserId, "hints", counts, offset, "nom", "bdate")
+	for _, friend := range friends {
+		if friend.Bdate == "" {
+			continue
+		}
+		DMY := strings.Split(friend.Bdate, ".")
+		year := ""
+		if len(DMY) == 2 {
+			year = "Unknown"
+		} else {
+			year = DMY[2]
+		}
+		month := DMY[1]
+		if _, ok := zeroNum[month]; ok {
+			month = zeroNum[month]
+		}
+		day := DMY[0]
+		if _, ok := zeroNum[day]; ok {
+			day = zeroNum[day]
+		}
+		user := ND{
+			Name: fmt.Sprintf("%v %v", friend.FirstName, friend.LastName),
+			Date: now + month + day,
+			Year: year,
+		}
+		//		fmt.Fprintf(w, "%v %v\n", friend.FirstName, friend.LastName)
+		//		fmt.Fprintf(w, friend.Bdate+"\n")
+		users = append(users, user)
+	}
+	//	fmt.Println(users)
+	file, err := os.Create("kek.ics")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	fields := h.IcsFields
+	writer := bufio.NewWriter(file)
+	begin := "BEGIN:VCALENDAR\nVERSION:2.0\n"
+	end := "END:VCALENDAR"
+	writer.WriteString(begin)
+	for _, user := range users {
+		for _, field := range fields {
+			switch field {
+			case "BEGIN:":
+				writer.WriteString(field + "VEVENT")
+				writer.WriteString("\n")
+				continue
+			case "SUMMARY:":
+				writer.WriteString(field + user.Name + "'s B-Day")
+				writer.WriteString("\n")
+				continue
+			case "DTSTART;VALUE=DATE:":
+				writer.WriteString(field + user.Date)
+				writer.WriteString("\n")
+				continue
+			case "DTEND;VALUE=DATE:":
+				writer.WriteString(field + user.Date)
+				writer.WriteString("\n")
+				continue
+			case "RRULE:FREQ=YEARLY;UNTIL=":
+				writer.WriteString(field + "21190101")
+				writer.WriteString("\n")
+				continue
+			case "DESCRIPTION:":
+				writer.WriteString(field + "Year of Birth: " + user.Year)
+				writer.WriteString("\n")
+				continue
+			case "END:":
+				writer.WriteString(field + "VEVENT")
+				writer.WriteString("\n")
+				continue
+			default:
+				fmt.Println("BLYA", field)
+				return
+			}
+		}
+	}
+	writer.WriteString(end)
+	writer.Flush()
+
+	w.Header().Add("Content-Disposition", "Attachment")
+	content, err := ioutil.ReadFile("kek.ics")
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.ServeContent(w, r, "random.ics", time.Now(), bytes.NewReader(content))
 }
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter,r *http.Request) {
-	switch r.URL.Path{
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
 	case "/":
-		h.handleMain(w,r)
+		h.handleMain(w, r)
 	case "/login":
 		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 	case "/result":
-		h.handleResult(w,r)
+		h.handleResult(w, r)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 	}
 }
 
-func main(){
-	client, err := vkapi.NewClientFromToken(TOKEN)
-	if err != nil {
-	    fmt.Println(err)
+func main() {
+	fields := []string{"BEGIN:", "SUMMARY:", "DTSTART;VALUE=DATE:", "DTEND;VALUE=DATE:", "RRULE:FREQ=YEARLY;UNTIL=", "DESCRIPTION:", "END:"}
+	handler := &Handler{
+		IcsFields: fields,
 	}
-	var userId int64 = 31567954
-	/*
-	hints - sort by rating
-	nom - Nominative
-	*/
-	var offset int64 = 0
-	var counts int64 = 5000
-	friends, err := client.GetFriends(userId, "hints", counts, offset, "nom", "bdate")
-	for _, friend := range friends{
-		fmt.Println(friend.FirstName)
-		fmt.Println(friend.LastName)
-		fmt.Println(friend.Bdate)
-	}
-	
-	handler := &Handler{}
 	port := "8080"
 	fmt.Println("Start listening at port: ", port)
 	http.ListenAndServe(":"+port, handler)
-//	values := url.Values{}
-//	values.Set("user_id", "31567954")
-//	values.Set("count", "10")
-//
-//	res, err := client.Do(vkapi.NewRequest("friends.getOnline", "", values))
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//
-//	fmt.Println(res.Response.String())
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+A little project, which let you get file *.ics with your vk-friends B-days.
